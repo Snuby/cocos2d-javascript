c4fcb8caeea5f9828b0cb649d3564ad563b0beac
Ryan Williams <ryan@wigg.ly>
2011-11-27 11:56:22 +1300

    Updated to support latest Jah

--- skeletons/cocos2d/src/main.js
+++ skeletons/cocos2d/src/main.js
@@ -4,7 +4,7 @@ var cocos = require('cocos2d')   // Import the cocos2d module
   , geo   = require('geometry')  // Import the geometry module
   , ccp   = geo.ccp              // Short hand to create points
 
-var $classname$ = cocos.nodes.Layer.extend(/** @lends $classname$# */{
+var ${classname} = cocos.nodes.Layer.extend(/** @lends ${classname}# */{
     /**
      * @class Initial application layer
      * @extends cocos.nodes.Layer
@@ -12,13 +12,13 @@ var $classname$ = cocos.nodes.Layer.extend(/** @lends $classname$# */{
      */
     init: function () {
         // You must always call the super class version of init
-        $classname$.superclass.init.call(this)
+        ${classname}.superclass.init.call(this)
 
         // Get size of canvas
         var s = cocos.Director.get('sharedDirector.winSize')
 
         // Create label
-        var label = cocos.nodes.Label.create({ string: '$appname$', fontName: 'Arial', fontSize: 76 })
+        var label = cocos.nodes.Label.create({ string: '${appname}', fontName: 'Arial', fontSize: 76 })
 
         // Add label to layer
         this.addChild({ child: label, z:1 })
@@ -38,13 +38,13 @@ exports.main = function () {
     var director = cocos.Director.get('sharedDirector')
 
     // Attach director to our <div> element
-    director.attachInView(document.getElementById('$filename$_app'))
+    director.attachInView(document.getElementById('${filename}_app'))
 
     // Create a scene
     var scene = cocos.nodes.Scene.create()
 
     // Add our layer to the scene
-    scene.addChild({ child: $classname$.create() })
+    scene.addChild({ child: ${classname}.create() })
 
     // Run the scene
     director.runWithScene(scene)
